a9db0f094e117ece714b90e80748280bcd726bd9
dirkneum <dirkneum2@gmail.com>
2022-04-24 18:06:40 +0200

    Added some cleanup steps, and regression.

--- serve.py
+++ serve.py
@@ -5,10 +5,13 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn.linear_model import Ridge
 import streamlit as st
 
 
 def field_value(f, fn):
+    if f == None:
+        return '?'
     if fn not in f:
         return '-'
     if f[fn] == '':
@@ -16,7 +19,7 @@ def field_value(f, fn):
     return f[fn]
 
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data(results=sys.maxsize, medicinalproduct_id=None):
     drugadministrationroute = []
     drugs = []
@@ -31,11 +34,17 @@ def load_data(results=sys.maxsize, medicinalproduct_id=None):
             x = json.load(f)
             for i in range(len(x['results'])):
                 result = x['results'][i]
+                if i == 0:
+                    print(result)
+                if field_value(result['primarysource'], 'qualification') not in ('1', '2', '3'):
+                    continue
                 patient = result['patient']
                 if medicinalproduct_id is not None and medicinalproduct_id not in [z['medicinalproduct'] for z in
                                                                                    patient['drug']]:
                     continue
                 for drug in patient['drug']:
+                    if field_value(drug, 'drugcharacterization') != '1':
+                        continue
                     for reaction in patient['reaction']:
                         if field_value(drug, 'medicinalproduct') == medicinalproduct_id:
                             continue
@@ -58,21 +67,27 @@ def load_data(results=sys.maxsize, medicinalproduct_id=None):
                          'reportercountry': reportercountry,
                          'safetyreportid': safetyreportid})
 
-results = int(st.text_input('Number of results', '1000'))
 
+results = int(st.text_input('Number of results', '1000'))
+st.info(
+    f'At most {results} entries from FAERS data are loaded. Please increase the above number to see more results.\n\n'
+    'Records from health care professionals (qualification = 1, 2 or 3) and where the drug'
+    'was considered responsible for the reaction (drugcharacterization = 1) are included.')
 # load json into dataframe
 df = load_data(results)
 
 #
 st.markdown('### Product by Reaction')
 df2 = df.groupby(['medicinalproduct', 'reactionmeddrapt']).nunique(
-    'safetyreportid').reset_index().sort_values('safetyreportid', ascending=False)
+    'safetyreportid').reset_index().sort_values('safetyreportid', ascending=False)[
+    ['medicinalproduct', 'reactionmeddrapt', 'safetyreportid']]
 st.write(df2[:100])
 
 # bar plot
 st.markdown('### Reaction Frequency')
 df2a = df.groupby(['reactionmeddrapt']).nunique('safetyreportid').reset_index().sort_values('safetyreportid',
-                                                                                            ascending=False)
+                                                                                            ascending=False)[
+    ['reactionmeddrapt', 'safetyreportid']]
 st.write(df2a)
 
 fig, ax = plt.subplots()
@@ -86,11 +101,13 @@ st.pyplot(fig)
 st.markdown('### Product by Country')
 
 reportercountries = df.groupby('reportercountry').nunique('safetyreportid').sort_values('safetyreportid',
-                                                                                        ascending=False).reset_index()
+                                                                                        ascending=False).reset_index()[
+    'reportercountry']
 reportercountry = st.selectbox('Select a country', reportercountries)
 
 df3 = df[df['reportercountry'] == reportercountry].groupby(['medicinalproduct']).nunique(
-    'safetyreportid').sort_values('safetyreportid', ascending=False).head(10).reset_index().dropna()
+    'safetyreportid').sort_values('safetyreportid', ascending=False).head(10).reset_index()[
+    ['medicinalproduct', 'safetyreportid']]
 st.write(df3)
 
 #
@@ -101,14 +118,26 @@ medicinalproducts = df.groupby('medicinalproduct').nunique('safetyreportid').sor
 medicinalproduct = st.selectbox('Select a medicinal product', medicinalproducts)
 
 df4 = load_data(results, medicinalproduct).groupby(['medicinalproduct']).nunique(
-    'safetyreportid').reset_index().sort_values('safetyreportid', ascending=False)
+    'safetyreportid').reset_index().sort_values('safetyreportid', ascending=False)[
+    ['medicinalproduct', 'safetyreportid']]
 st.write(df4)
 
-min_drugs = st.slider('Minimum number of drugs', 0, 10)
-df5 = df[df['drugs'] >= min_drugs].groupby(['reactionmeddrapt']).nunique('safetyreportid').reset_index().sort_values('safetyreportid', ascending=False)
+st.markdown('### Multiple Drugs - Reactions')
+df5 = df
+df5['safetyreportid_reactionmeddrapt'] = df5['safetyreportid'] + '_' + df5['reactionmeddrapt']
+df5 = df5.groupby(['drugs']).nunique(['safetyreportid', 'safetyreportid_reactionmeddrapt']).reset_index().sort_values(
+    'safetyreportid', ascending=False)[['drugs', 'safetyreportid', 'safetyreportid_reactionmeddrapt']]
+df5['reactionmeddrapt/safetyreportid'] = df5['safetyreportid_reactionmeddrapt'] / df5['safetyreportid']
 st.write(df5)
 
 fig, ax = plt.subplots()
-g = sns.barplot(x="drugs", y="safetyreportid", data=df5)
+g = sns.barplot(x="drugs", y="reactionmeddrapt/safetyreportid", data=df5)
 g.set_xticklabels(g.get_xticklabels(), rotation=90)
 st.pyplot(fig)
+
+max_drugs = st.slider('Max Drugs', 1, 50, 10)
+df5 = df5[df5['drugs'] <= max_drugs]
+X = df5['drugs'].values.reshape(-1, 1)
+y = df5['reactionmeddrapt/safetyreportid']
+reg = Ridge().fit(X, y)
+st.write(f'Ridge regression weight vector: {reg.coef_[0]:.3} (intercept: {reg.intercept_:.3}).')
